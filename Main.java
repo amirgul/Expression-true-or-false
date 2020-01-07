63b3da9b8af0041d222043f19ca1a732fc25e5cf
Amir Gul <amirgul@Amirs-MacBook-Pro.local>
2020-01-07 13:57:17 -0500

    Ten

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+
+        int x, y, z;
+        x = 5;
+        y = 10;
+        z = 15;
+        boolean answer = (!(x>3) && x!=z ||x+y ==z);
+        System.out.println(answer);
+    }
+}
